2c37c79f867d1c9ae5f5cb8612a1f73df54d6dbc
Adarshg315 <adarshg315@gmail.com>
2023-02-18 16:38:42 +0530

    added token revalidator

--- app/root.tsx
+++ app/root.tsx
@@ -1,4 +1,5 @@
-import { json, LoaderArgs, MetaFunction } from "@remix-run/node";
+import type { LoaderArgs, MetaFunction } from "@remix-run/node";
+import { json } from "@remix-run/node";
 import {
   Links,
   LiveReload,
@@ -7,12 +8,11 @@ import {
   Scripts,
   ScrollRestoration,
   useLoaderData,
+  useRevalidator,
 } from "@remix-run/react";
-import { SupabaseClient } from "@supabase/supabase-js";
+import type { SupabaseClient } from "@supabase/supabase-js";
 import { useEffect, useState } from "react";
-
 import createServerSupabase from 'utils/supabase.server';
-
 import { createBrowserClient } from "@supabase/auth-helpers-remix";
 import type { Database } from "db_types";
 
@@ -46,19 +46,29 @@ export const loader = async ({ request }: LoaderArgs) => {
 
 export default function App() {
   const { env, session } = useLoaderData<typeof loader>();
-
-  console.log({ server: { session } });
-
-
+  const revalidator = useRevalidator()
 
   const [supabase] = useState(() =>
     createBrowserClient<Database>(env.SUPABASE_URL, env.SUPABASE_ANON_KEY)
   );
 
 
+  const serverAccessToken = session?.access_token
+
+
   useEffect(() => {
-    supabase.auth.getSession().then((session) => console.log({ client: { session } }))
-  }, []);
+    const { data: { subscription } } = supabase.auth.onAuthStateChange((event, session) => {
+      if (session?.access_token !== serverAccessToken) {
+        //call loaders
+        revalidator.revalidate()
+      }
+    })
+
+
+    return () => {
+      subscription.unsubscribe()
+    }
+  }, [revalidator, serverAccessToken, supabase]);
 
 
 
